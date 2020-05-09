cabea6052bc283ef4bb99c4ac3f61c5a43920f98
Pablo Pioli <15127047+pablopioli@users.noreply.github.com>
2020-05-09 20:09:00 -0300

    Change type data of QueryResult

--- src/MoonGrid/QueryResult.cs
+++ src/MoonGrid/QueryResult.cs
@@ -1,19 +1,21 @@
-﻿namespace MoonGrid
+﻿using System.Collections.Generic;
+
+namespace MoonGrid
 {
     public class QueryResult<T>
     {
-        public T[] ResultData { get; set; }
+        public ICollection<T> ResultData { get; set; }
         public bool HasMoreData { get; set; }
 
         public QueryResult()
         { }
 
-        public QueryResult(T[] resultData)
+        public QueryResult(ICollection<T> resultData)
         {
             ResultData = resultData;
         }
 
-        public QueryResult(T[] resultData, bool hasMoreData)
+        public QueryResult(ICollection<T> resultData, bool hasMoreData)
         {
             ResultData = resultData;
             HasMoreData = hasMoreData;
